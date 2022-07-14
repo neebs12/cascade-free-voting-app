79ecafb4139a7ebcbd2ac66a011e6e4c151b1d9b
Jason Aricheta <jason.aricheta@gmail.com>
2022-07-15 10:21:26 +1200

    made P1 route working

--- server/routes/sessions.js
+++ server/routes/sessions.js
@@ -1,15 +1,28 @@
 const express = require('express')
 
-const db = require('../db/db')
+const db = require('../db/dbfunctions/db')
+const dbSessions = require('../db/dbfunctions/sessions')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.status(200).json({message: 'G_NA: sessions GET routes not yet implemented'})
+router.get('/', async (req, res) => {
+  // res.status(200).json({message: 'G_NA: sessions GET routes not yet implemented'})
+  const result = await db.getByTableName('sessions')
+  res.status(200).json(result)
 })
 
-router.post('/', (req, res) => {
-  res.status(200).json({message: 'P1: sessions routes not yet implemented'})
+router.post('/', async (req, res) => {
+  try {
+    const rawData = req.body
+    await dbSessions.addToSession({
+      eventName: rawData.title,
+      winningIdeasNum: rawData.numWinners
+    })
+    
+    res.status(204).end()
+  } catch {
+    res.status(500).json({message: 'unable to add session'})
+  }
 })
 
 module.exports = router
