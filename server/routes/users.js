a63b9b5c2300d4496b26340b4c43b094bbc3636a
Jason Aricheta <jason.aricheta@gmail.com>
2022-07-15 10:39:13 +1200

    completed G2 route

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,8 +4,23 @@ const db = require('../db/dbfunctions/db')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.status(200).json({message: 'users routes not yet implemented'})
+router.get('/', async (req, res) => {
+  /* Expected information
+  [
+    {
+     id,
+     name,
+    },
+    {...}, 
+    {...}, ....
+  ]
+  */
+  // 
+  const result = await db.getByTableName('users')
+  // results look like: [{id: '...', name: '...', voted: '...'}, {...}, ...]
+  // simply deletes the `voted` property from the `r` object
+  result.forEach(r => { delete r.voted })
+  res.status(200).json(result)
 })
 
 module.exports = router
