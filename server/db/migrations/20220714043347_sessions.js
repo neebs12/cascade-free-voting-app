11939fb9922b8c2118a7e5b3c7dce70bd2eaa324
Jason Aricheta <jason.aricheta@gmail.com>
2022-07-14 16:38:45 +1200

    added additional sessions table migration

---
+++ server/db/migrations/20220714043347_sessions.js
@@ -0,0 +1,13 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('sessions', function(table) {
+    // useful for A1 form
+    // this is the event_name
+    table.string('event_name')
+    // this is the winning_ideas_num
+    table.int('winning_ideas_num')
+  })
+}
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('sessions')
+}
