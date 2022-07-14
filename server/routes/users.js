c78a313deeefba31d2e84f77754d4fe037853a7f
Jason Aricheta <jason.aricheta@gmail.com>
2022-07-15 10:49:52 +1200

    completed P2 -- adding new users

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,22 +5,22 @@ const db = require('../db/dbfunctions/db')
 const router = express.Router()
 
 router.get('/', async (req, res) => {
-  /* Expected information
-  [
-    {
-     id,
-     name,
-    },
-    {...}, 
-    {...}, ....
-  ]
-  */
-  // 
   const result = await db.getByTableName('users')
   // results look like: [{id: '...', name: '...', voted: '...'}, {...}, ...]
   // simply deletes the `voted` property from the `r` object
-  result.forEach(r => { delete r.voted })
+  result.forEach(r => delete r.voted)
   res.status(200).json(result)
+})
+
+router.post('/', async (req, res) => {
+  /* Expected: 
+  {
+    "name": string
+  }
+  */
+  const result = req.body
+  await db.addByTableName('users', result)
+  res.status(201).end() 
 })
 
 module.exports = router
