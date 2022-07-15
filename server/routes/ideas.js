4a8c79a0064bf2c11d19b1bb23d31f17383d6da6
Jason Aricheta <jason.aricheta@gmail.com>
2022-07-15 16:45:11 +1200

    completed G7

--- server/routes/ideas.js
+++ server/routes/ideas.js
@@ -1,11 +1,15 @@
 const express = require('express')
 
 const db = require('../db/dbfunctions/db')
+const dbIdeas = require('../db/dbfunctions/ideas')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.status(200).json({message: 'ideas routes not yet implemented'})
+router.get('/', async (req, res) => {
+  const result = await dbIdeas.getIdeasWithOwnerAndVotes()
+  // console.log(result)
+   
+  res.status(200).json(result)
 })
 
 module.exports = router
--- server/routes/votes.js
+++ server/routes/votes.js
@@ -19,4 +19,20 @@ router.get('/', async (req, res) => {
   res.status(200).json(result)
 })
 
+router.get('/is_ready', async (req, res) => {
+  // need to see if all users have existing ideas
+  // need all users, get respective ids
+  const allUserIds = (await db.getByTableName('users'))
+                              .map(u => u.id)
+  // need all ideas, get respective user_ids (no need to be unique)
+  const userIdsInIdeas = (await db.getByTableName('ideas'))
+                                  .map(idea => idea.id)
+  // filter `allUserIds` - `filteredUserId`
+  // -- iterate through `allUserIds`
+  // -- if an id exists within `userIdsInIdeas`, return false, else true
+  // `booln` = filteredUserId.length === 0, return true, else false {votingReady: bool} 
+
+
+})
+
 module.exports = router
