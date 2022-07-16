aad47efb018aa06898b3fce7413ee445a38f4d4e
Graeme Ford <gkford@gmail.com>
2022-07-16 12:57:08 +1200

    get redux toolkit basic setup working

--- client/components/Ideas.jsx
+++ client/components/Ideas.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { selectAllIdeas, fetchIdeas } from '../features/ideas/ideasSlice'
+import { useSelector, useDispatch } from 'react-redux'
+import { add } from '../features/ideas/ideasSlice'
 
 export default function Ideas () {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    console.log('use effect')
+    dispatch(fetchIdeas())
+    dispatch(add())
+  }, [])
+
+  const ideas = useSelector(selectAllIdeas)
+
   return (
     <>
       <h1>A2</h1>
+
+      {JSON.stringify(ideas)}
       <h2>This the page where the admin enters all the ideas</h2>
     </>
   )
