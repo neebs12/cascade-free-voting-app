dffc2ed23ff9f2ea09660868263d77040c2a0a13
jared-sukha <jared.sukha@gmail.com>
2022-07-18 16:48:24 +1200

    show results button works as per user story

--- client/components/AdminWaiting.jsx
+++ client/components/AdminWaiting.jsx
@@ -1,61 +1,78 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Button from '@mui/material/Button'
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import {
+  fetchUsersStatus,
+  fetchUsers,
+  selectUserStatus
+} from '../features/users/usersSlice'
+import { useSelector, useDispatch } from 'react-redux'
 
-export default function AdminWaiting() {
-  const user_status = {
-    voted: [
-      { id: 1, name: 'John' },
-      { id: 2, name: 'Paul' },
-      { id: 3, name: 'Ringo' },
-    ],
-    notVoted: [
-      { id: 1, name: 'Leo' },
-      { id: 2, name: 'Raph' },
-      { id: 3, name: 'Mikey' },
-    ],
-  }
+export default function AdminWaiting () {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const [voteSubmit, setVoteSubmit] = useState()
+
+  useEffect(() => {
+    dispatch(fetchUsers())
+    dispatch(fetchUsersStatus())
+  }, [])
+
+  // const voted = useSelector(nameofselector)
+
+  const handleClick = () => navigate('/admin/results', { replace: true })
+
+  const user_status = useSelector(selectUserStatus)
+
+  useEffect(() => {
+    if (user_status?.notVoted.length === 0) {
+      setVoteSubmit(true)
+    } else {
+      setVoteSubmit(false)
+    }
+  }, [user_status])
 
   return (
     <>
-      <h1>A3</h1>
-      <h2>
-        This is the page where the admin waits for the voting to finish and can
-        see who still is left to vote
-      </h2>
-      <div>
-        <Button variant="outlined">Reload</Button>
-      </div>
-      <div className="admin-waiting-vote-table">
-        <div className="admin-waiting-vote-col">
-          <h3>Still waiting to submit vote</h3>
-          {user_status.voted.map((name) => {
-            return (
-              <div className="admin-waiting-vote-name" key={name.id}>
-                {name.name}
-              </div>
-            )
-            // return <IdeaTile key={idea.id} idea={idea} />
-          })}
-        </div>
-        <div className="admin-waiting-vote-col">
-          <h3>Voting completed</h3>
-          {user_status.notVoted.map((name) => {
-            return (
-              <div className="admin-waiting-vote-name" key={name.id}>
-                {name.name}
-              </div>
-            )
-            // return <IdeaTile key={idea.id} idea={idea} />
-          })}
-        </div>
-      </div>
-      <Button variant="contained" disabled>
-        Show Results
-      </Button>{' '}
-      <Button component={Link} to="/admin/results" variant="outlined">
-        Show Results
-      </Button>
+      {user_status && (
+        <>
+          <h1>A3</h1>
+          <h2>
+            This is the page where the admin waits for the voting to finish and
+            can see who still is left to vote
+          </h2>
+          <div>
+            <Button variant="outlined">Reload</Button>
+          </div>
+          <div className="admin-waiting-vote-table">
+            <div className="admin-waiting-vote-col">
+              <h3>Still waiting to submit vote</h3>
+              {user_status.voted.map((name) => {
+                return (
+                  <div className="admin-waiting-vote-name" key={name.id}>
+                    {name.name}
+                  </div>
+                )
+                // return <IdeaTile key={idea.id} idea={idea} />
+              })}
+            </div>
+            <div className="admin-waiting-vote-col">
+              <h3>Voting completed</h3>
+              {user_status.notVoted.map((name) => {
+                return (
+                  <div className="admin-waiting-vote-name" key={name.id}>
+                    {name.name}
+                  </div>
+                )
+                // return <IdeaTile key={idea.id} idea={idea} />
+              })}
+            </div>
+          </div>
+          <Button variant="contained" disabled={voteSubmit} onClick={handleClick}>
+            Show Results
+          </Button>
+        </>
+      )}
     </>
   )
 }
