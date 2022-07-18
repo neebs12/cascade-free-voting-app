85e9c506210c8073f5bbbaad38a09faa8a6df4f5
Graeme Ford <gkford@gmail.com>
2022-07-18 14:31:38 +1200

    actually uS20 this time

--- client/components/AfterVote.jsx
+++ client/components/AfterVote.jsx
@@ -1,25 +1,20 @@
 import React, { useEffect } from 'react'
 import Button from '@mui/material/Button'
-import { Link } from 'react-router-dom'
 import CircularProgress from '@mui/material/CircularProgress'
 import Box from '@mui/material/Box'
-import { useSelector, useDispatch } from 'react-redux'
-import {
-  selectAllIdeas,
-  fetchIdeas,
-  selectVoteReady
-} from '../features/ideas/ideasSlice'
-import { fetchSession } from '../features/session/sessionSlice'
-import { fetchUsersStatus, fetchUsers } from '../features/users/usersSlice'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { selectResultsReady } from '../features/users/usersSlice'
+import { fetchAllUsersStatus } from '../apis/users'
 
 export default function AfterVote () {
   const dispatch = useDispatch()
 
   useEffect(() => {
-    dispatch(fetchUsers())
-    dispatch(fetchUsersStatus())
+    dispatch(fetchAllUsersStatus)
   }, [])
 
+  const resultsReady = useSelector(selectResultsReady)
   return (
     <>
       <h1>U4</h1>
@@ -27,6 +22,14 @@ export default function AfterVote () {
         This is the user waiting page after the vote - waiting for all users to
         finish voting
       </h2>
+      {!resultsReady && (
+        <Box sx={{ display: 'flex' }}>
+          <CircularProgress />
+        </Box>
+      )}
+      <Button variant="contained" onClick={window.location.reload}>Reload</Button>
+      {/* Better fix the sneaky p tags here */}
+      <p></p>
       <Button variant="contained" disabled>
         Proceed to Results
       </Button>{' '}
